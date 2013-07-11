ddd7012cf2bd78b4ef4c515f82226019b75c7e18
pdoran <patrick.doran@gmail.com>
2013-07-11 09:58:02 -0400

    More style changes, created template and start/next/back

--- index.js
+++ index.js
@@ -1,4 +1,5 @@
 /*global module:false,require:false,console:false */
+var fs = require('fs');
 var webroot = "./server";
 var port = 8000;
 var assetTypes = [".js", ".css", ".txt", ".ico", ".html", ".png",".jpg",".json"];
@@ -27,7 +28,7 @@ module.exports.startServer = function() {
 
       file.serve(req, res, function(err, result) {
         if (err) {
-          console.error("Error serving %s - %s", req.url, err.message);
+          //console.error("Error serving %s - %s", req.url, err.message);
           res.writeHead(err.status, err.headers);
           res.end();
         }
@@ -47,11 +48,10 @@ process.stdin.on('data', function(chunk) {
   checkCommands(chunk);
 });
 
-var currentItem = 1;
+var currentItem = 0;
 
 var checkCommands = function(chunk) {
   var args = chunk.split(" ");
-  console.log(args);
   switch(args[0].replace("\r\n","")) {
     case "clone":
       clone(args);
@@ -59,14 +59,26 @@ var checkCommands = function(chunk) {
     case "next":
       next(args);
       break;
+    case "start":
+      start();
+      break;
+    case "back":
+      back();
+      break;
     default:
       process.stdout.write('Cannot find command for: ' + chunk);
       break;
     }
 };
 
-var clone = function(args) {
+var start = function() {
+  next([]);
+}
 
+var clone = function(args) {
+  var file = args[1].replace("\r\n","");
+  fs.renameSync("./server/index.html","./server/index.html.bak");
+  fs.createReadStream("./server/"+file).pipe(fs.createWriteStream("./server/index.html"));
 }
 var next = function(args) {
   if(args.length>1) {
@@ -74,7 +86,12 @@ var next = function(args) {
   } else {
     currentItem++;  
   }
+  clone(["clone", currentItem+".html"]);
   console.log("Moving to item " + currentItem);
+}
+var back = function() {
+  currentItem--;
+  fs.renameSync("./server/index.html.bak","./server/index.html");
 }
 
 process.stdin.on('end', function() {
