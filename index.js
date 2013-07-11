c3e84b2a38f7ad1516ccce460569d6c1da8e2d25
Patrick Doran <patrick.doran@gmail.com>
2013-07-10 21:34:48 -0400

    Creating presentation logic

--- index.js
+++ index.js
@@ -21,7 +21,6 @@ module.exports.startServer = function() {
   http.createServer(function(req, res) {
   	req.on('data', function() {});
     req.addListener("end", function() {
-    	console.log(req.url);
       if (!isStaticResource(req.url)) {
         req.url = "/index.html";
       }
@@ -40,3 +39,44 @@ module.exports.startServer = function() {
 };
 
 module.exports.startServer();
+
+process.stdin.resume();
+process.stdin.setEncoding('utf8');
+
+process.stdin.on('data', function(chunk) {
+  checkCommands(chunk);
+});
+
+var currentItem = 1;
+
+var checkCommands = function(chunk) {
+  var args = chunk.split(" ");
+  console.log(args);
+  switch(args[0].replace("\r\n","")) {
+    case "clone":
+      clone(args);
+      break;
+    case "next":
+      next(args);
+      break;
+    default:
+      process.stdout.write('Cannot find command for: ' + chunk);
+      break;
+    }
+};
+
+var clone = function(args) {
+
+}
+var next = function(args) {
+  if(args.length>1) {
+    currentItem = parseInt(args[1],10);
+  } else {
+    currentItem++;  
+  }
+  console.log("Moving to item " + currentItem);
+}
+
+process.stdin.on('end', function() {
+  process.stdout.write('end');
+});
